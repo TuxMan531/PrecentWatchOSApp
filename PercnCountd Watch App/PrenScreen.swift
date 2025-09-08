1ff901e75288b90f2dc85f31e642f3a982a2b4a4
TuxMan531 <jaxf2005@gmail.com>
2025-09-08 18:41:41 -0500

    droc101 genius inovation

--- PercnCountd Watch App/PrenScreen.swift
+++ PercnCountd Watch App/PrenScreen.swift
@@ -17,8 +17,8 @@ struct PercentScreen: View {
             .font(.title2)
             .monospacedDigit()
             .padding()
-            .onReceive(Timer.publish(every: 0.016667, on: .main, in: .common).autoconnect()) { now = $0 }
+            .onReceive(Timer.publish(every: 1/60, on: .main, in: .common).autoconnect()) { now = $0 }
         
-        // 0.016667 is 60fps
+        // 60fps is 0.016667s hello to my good friend droc101
     }
 }
