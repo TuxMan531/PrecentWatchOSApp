63eba43ec877bec94813d395d9d671047ca7f0e7
TuxMan531 <jaxf2005@gmail.com>
2025-09-07 21:15:18 -0500

    Initial Commit

---
+++ PercnCountd Watch App/PercnCountdApp.swift
@@ -0,0 +1,17 @@
+//
+//  PercnCountdApp.swift
+//  PercnCountd Watch App
+//
+//  Created by TuxMan on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PercnCountd_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PercnCountd Watch AppTests/PercnCountd_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PercnCountd_Watch_AppTests.swift
+//  PercnCountd Watch AppTests
+//
+//  Created by TuxMan on 9/7/25.
+//
+
+import Testing
+@testable import PercnCountd_Watch_App
+
+struct PercnCountd_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
